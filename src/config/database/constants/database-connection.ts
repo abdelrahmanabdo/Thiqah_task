4424bc8e1fd7308d00144475ae12bda8b1f6e2cf
Abdo Salmon <abdo@bodecanada.com>
2023-01-18 23:55:57 +0200

    Add nestjs upload file code

--- src/config/database/constants/database-connection.ts
+++ src/config/database/constants/database-connection.ts
@@ -1,10 +1,9 @@
-export const DatabaseConnection: any = {
-  type: 'mysql',
+import { AuroraMysqlConnectionCredentialsOptions } from 'typeorm/driver/aurora-mysql/AuroraMysqlConnectionCredentialsOptions';
+
+export const DatabaseConnection: AuroraMysqlConnectionCredentialsOptions = {
   host: process.env.DBHOST,
   port: Number(process.env.DBPORT),
   username: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   database: process.env.DBDATABASE,
-  logging: false,
-  synchronize: false,
 };
